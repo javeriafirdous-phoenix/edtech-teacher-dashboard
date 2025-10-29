ae00e027b3dd720d874f8d073f47877b5f946a26
Javeria <j.f.rumana@gmail.com>
2025-10-29 14:45:48 +0530

    Add .gitignore to hide .env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,15 +1,18 @@
+
+
+  // src/firebase-config.js
 import { initializeApp } from "firebase/app";
-import {getFirestore} from '@firebase/firestore';
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-   apiKey: "AIzaSyBlVyIIVnMidKzrXkLmYUC_HBJCMEE5BtY",
-  authDomain: "edtech-prototype-1058c.firebaseapp.com",
-  projectId: "edtech-prototype-1058c",
-  storageBucket: "edtech-prototype-1058c.firebasestorage.app",
-  messagingSenderId: "583937555367",
-  appId: "1:583937555367:web:acd95c78e83e2b235cd482",
-  measurementId: "G-JQQCRS8DYQ"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
-  const app = initializeApp(firebaseConfig)
-
-  export const db= getFirestore(app);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
